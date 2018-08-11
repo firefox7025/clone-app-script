9c97baf9b6ff1d47526b9dd02707fcab1b93437d
Alexander Montgomery <evaderxander@gmail.com>
2018-08-11 18:20:18 -0400

    Added org support

--- clone.py
+++ clone.py
@@ -8,33 +8,38 @@ from github3 import login
 API_PASSWORD = ''
 API_USER = ''
 url = 'https://api.github.com/'
-orgs = [""]
+org = None
 gh = login('', '')
 
 def setargs(args):
     global API_KEY
     global API_USER
     global gh
-    if args.api_key != None:
-        API_KEY = args.api_key
-        print("Would have set var:" + API_KEY)
+    global org
+    if args.password != None:
+        API_KEY = args.password
     if args.username != None:
         API_USER = args.username
-        print("Would have set var:" + API_USER)
     gh = login(API_USER, API_KEY)
-    
-def makeCloneFolder(org):
+    if args.org != None:
+        org = gh.organization(args.org)
+
+def makeCloneFolder(args):
     directory = os.getcwd() + "/repos"
-    if org != '':
-        directory += "/"+org
+    if args.org != None:
+        directory += "/"+args.org
+    else:
+        directory += "/" + args.username
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
 
-def getRepoList(directory, user):
-    repos = gh.repositories_by(user)
+def getRepoList(directory, user, args):
+    if args.org == None:
+        repos = gh.repositories_by(user)
+    else:
+        repos = org.repositories()
     for repo in repos:
-        print(repo.as_json)
         x = repo.as_dict()["clone_url"]
         cloneRepo(x, repo.name, directory)
 
@@ -42,21 +47,20 @@ def cloneRepo(repo, project_name, directory):
     auth = "https://" + API_USER + ':' + API_KEY + '@'
     url = auth + repo.replace('"', '').split("https://")[1]
     print("cloning ", project_name)
-    cloneCommand = "git clone " + url + " " + directory + "/" + project_name
+    cloneCommand = "git clone --depth 1 " + url + " " + directory + "/" + project_name
     os.system(cloneCommand)
     
 def main(args):
     setargs(args)
     print(API_KEY + " : " + API_USER)
-    getRepoList(makeCloneFolder(API_USER), API_USER)
-    for org in orgs:
-        getRepoList(makeCloneFolder(org), org)
+    getRepoList(makeCloneFolder(args), API_USER, args)
     
 
 
 parser = argparse.ArgumentParser(description='Clone all repos from a github user')
 parser.add_argument('--password', help='The password of the github account')
 parser.add_argument('--username', help='The username of the github account')
+parser.add_argument('--org', help='Download all of the repos of a particular org')
 args = parser.parse_args()
 print(args)
 main(args)
